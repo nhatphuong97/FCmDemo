2eabd3a954a850a50df718abd924e8d479204ea1
ong-phuong <TV05.IT3@VNPT.VN>
2020-08-24 15:29:26 +0700

    Add notification Firebase

--- app/src/main/java/com/example/fcm/FCMService.kt
+++ app/src/main/java/com/example/fcm/FCMService.kt
@@ -1,22 +1,33 @@
 package com.example.fcm
 
+import android.content.Intent
 import android.util.Log
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+
 class FCMService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        val tokens =  FirebaseInstanceId.getInstance().token
+        val tokens = FirebaseInstanceId.getInstance().token
         Log.d("xxx", "11123          $tokens")
     }
 
     override fun onMessageReceived(rM: RemoteMessage) {
         super.onMessageReceived(rM)
+        val intent = Intent()
+        intent.apply {
+            action = BROADCAST_ACTION
+            putExtra("title", rM.notification?.body)
+        }
+            sendBroadcast(intent)
         Log.d("xxx", "11123          ${rM.notification?.body}")
 
     }
 
+    companion object {
+        const val BROADCAST_ACTION = "BROADCAST_ACTION"
+    }
 }
--- app/src/main/java/com/example/fcm/MainActivity.kt
+++ app/src/main/java/com/example/fcm/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.fcm
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.OnSuccessListener
+import androidx.core.app.NotificationCompat
 import com.google.firebase.iid.FirebaseInstanceId
-import com.google.firebase.iid.InstanceIdResult
 import com.google.firebase.messaging.FirebaseMessaging
 
 
@@ -14,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       FirebaseMessaging.getInstance().isAutoInitEnabled = true
+        FirebaseMessaging.getInstance().isAutoInitEnabled = true
 
         FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener(
             this
@@ -25,5 +34,66 @@ class MainActivity : AppCompatActivity() {
         //anh coi build 1 cai thu a genymotion a // bua ni ma con choi gen //masy e yeu qua
         //k chay may ao kia dc
 
+
+        if (mReciver != null) {
+//Create an intent filter to listen to the broadcast sent with the action "ACTION_STRING_ACTIVITY"
+            val intentFilter = IntentFilter(FCMService.BROADCAST_ACTION)
+            //Map the intent filter to the receiver
+            registerReceiver(mReciver, intentFilter)
+        }
     }
+
+    private val mReciver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == FCMService.BROADCAST_ACTION) {
+                Toast.makeText(
+                    applicationContext,
+                    "received message in activity..!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                buildNotifiction(intent.getStringExtra("title") ?: "xxx")
+            }
+
+        }
+    }
+
+    private fun buildNotifiction(Message: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        val contentIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val b = NotificationCompat.Builder(this, "xxxx")
+
+        b.setAutoCancel(true)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setWhen(System.currentTimeMillis())
+            .setSmallIcon(android.R.drawable.ic_media_next)
+            .setContentText(Message)
+            .setDefaults(Notification.DEFAULT_LIGHTS or Notification.DEFAULT_SOUND)
+            .setContentIntent(contentIntent)
+        val notificationManager =
+            this.getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            b.setStyle(NotificationCompat.BigTextStyle())
+        } else {
+            b.setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(getString(R.string.app_name))
+            ).setContentTitle(getString(R.string.app_name))
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = getString(R.string.app_name)
+            val notificationChannel = NotificationChannel(
+                "xxxx",
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+            notificationManager?.createNotificationChannel(notificationChannel)
+        }
+        notificationManager?.notify(1, b.build())
+    }
+
 }
